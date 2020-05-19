ab6fe3787c50857ba9520d761b8c7526e2cb26db
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-19 15:03:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,13 @@
 const playlist = {
-  artistName: songTitle
+  "Beyonce": "Crazy in Love"
 }
 
-function updatePlaylist({}, name, title) {
+function updatePlaylist(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
 
+function removeFromPlaylist(obj, key){
+  delete obj[key];
+  return obj;
 }
